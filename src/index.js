1095f676a1588a73b3165223edbf6ae3e253f907
Edward <Ecampbell3015@gmail.com>
2022-07-20 13:48:42 -0700

    finish 1st version of main content

--- src/index.js
+++ src/index.js
@@ -70,3 +70,50 @@ const searchIcon = document.createElement('i');
 searchIcon.className = 'fa-solid fa-magnifying-glass';
 topNav.appendChild(searchIcon);
 //Top Nav End----
+
+//Main Content ---
+const mainContentContainer = document.createElement('section');
+mainContentContainer.className = "mainContentContainer";
+content.appendChild(mainContentContainer);
+
+//Banner
+const banner = document.createElement('div');
+banner.className = "bannerWrapper";
+mainContentContainer.appendChild(banner);
+//Img--
+const bannerImg = document.createElement('img');
+bannerImg.src = "https://d1fd34dzzl09j.cloudfront.net/Images/CFACOM/Home%20Page%20Refresh/CFA_Icon_CFAOne2_Red_PMS_new.png?h=288&w=288&la=en";
+bannerImg.className = "bannerImg";
+banner.appendChild(bannerImg);
+
+//Text
+const bannerTextWrap = document.createElement('div');
+bannerTextWrap.className = "bannerTextWrap";
+banner.appendChild(bannerTextWrap);
+const makeEveryOrder = document.createElement('h3');
+makeEveryOrder.textContent = "Make every order count";
+bannerTextWrap.appendChild(makeEveryOrder);
+const joinTextWrap = document.createElement('div');
+joinTextWrap.className = "joinTextWrap";
+bannerTextWrap.appendChild(joinTextWrap);
+const join = document.createElement('h1');
+join.textContent = "Join Chick-Fil-A One"
+joinTextWrap.appendChild(join);
+ const reserved = document.createElement('i');
+ reserved.className = "fa-regular fa-registered";
+ joinTextWrap.appendChild(reserved);
+ const today = document.createElement('h1');
+ today.textContent = "today";
+ joinTextWrap.appendChild(today);
+
+ //Join Today
+const joinToday = document.createElement('div');
+joinToday.className = "joinToday";
+banner.appendChild(joinToday);
+const joinTodayText = document.createElement('p');
+joinTodayText.className = "joinTodayTextP";
+joinTodayText.textContent = "Join today";
+joinToday.appendChild(joinTodayText);
+const joinArrow = document.createElement('i');
+joinArrow.className = "fa-solid fa-angle-right";
+joinTodayText.appendChild(joinArrow);
